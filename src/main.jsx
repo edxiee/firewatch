3c09acaeb9a90f0a5297379ff38786f4c9f28e1b
Ed Christian Villanueva <edchristianvillanueva@gmail.com>
2026-02-27 21:17:17 +0800

    Pages folder renamed to Auth

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Auth from './pages/Auth.jsx'; // Corrected path based on your explorer
+import Auth from './Auth/Auth.jsx'; // Corrected path based on your explorer
 import './index.css';
 
 const hideSplashScreen = () => {
